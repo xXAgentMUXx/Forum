a38024c6c0b15870963fc0edaf09a167dcda3a50
Mathys <urban.mathys@gmail.com>
2025-02-28 10:39:27 +0100

    Forum

--- go.mod
+++ go.mod
@@ -3,7 +3,9 @@ module Forum
 go 1.24.0
 
 require (
-	github.com/mattn/go-sqlite3 v1.14.24 // indirect
+	github.com/google/uuid v1.6.0
+	github.com/gorilla/mux v1.8.1
+	github.com/mattn/go-sqlite3 v1.14.24
 	github.com/satori/go.uuid v1.2.0 // indirect
-	golang.org/x/crypto v0.35.0 // indirect
+	golang.org/x/crypto v0.35.0
 )
--- server/main.go
+++ server/main.go
@@ -1,19 +1,65 @@
 package main
 
 import (
-	"Forum"
+	"database/sql"
+	"encoding/json"
+	"fmt"
+	"log"
 	"net/http"
-	"time"
+
+	_"github.com/mattn/go-sqlite3"
+	"github.com/gorilla/mux"
+	"Forum/auth"
 )
-type Index struct {
-	ThreadSubject   string
-	ThreadCreatedAt time.Time
-	ThreadUUID      string
-	Username        string
-	PostCount       int
-}
+
+var db *sql.DB
 
 func main() {
-	mux := http.NewServeMux()
-	mux.HandleFunc("/forum", Forum.Forum)
+	var err error
+	db, err = sql.Open("sqlite3", "./forum.db")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	
+	r := mux.NewRouter()
+
+	r.HandleFunc("/register", RegisterHandler).Methods("POST")
+	r.HandleFunc("/login", LoginHandler).Methods("POST")
+
+	fmt.Println("Serveur démarré sur :8080")
+	log.Fatal(http.ListenAndServe(":8080", r))
+}
+
+func RegisterHandler(w http.ResponseWriter, r *http.Request) {
+	var user auth.User
+	err := json.NewDecoder(r.Body).Decode(&user)
+	if err != nil {
+		http.Error(w, "Données invalides", http.StatusBadRequest)
+		return
+	}
+	err = auth.RegisterUser(db, user.Email, user.Username, user.Password)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(map[string]string{"message": "Register Complete"})
+}
+func LoginHandler(w http.ResponseWriter, r *http.Request) {
+	var creds struct {
+		Email    string `json:"E-mail"`
+		Password string `json:"Password"`
+	}
+	err := json.NewDecoder(r.Body).Decode(&creds)
+	if err != nil {
+		http.Error(w, "Invalides Data", http.StatusBadRequest)
+		return
+	}
+	err = auth.LoginUser(db, creds.Email, creds.Password, w)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusUnauthorized)
+		return
+	}
 }
