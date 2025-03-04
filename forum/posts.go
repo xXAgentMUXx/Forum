e720362933f49318586f8d1c2eacfab5aa1f1bb3
Mathys <urban.mathys@gmail.com>
2025-03-04 18:19:21 +0100

    Bug fix category

--- forum/posts.go
+++ forum/posts.go
@@ -106,7 +106,6 @@ func GetAllPosts(w http.ResponseWriter, r *http.Request) {
         }
         posts = append(posts, post)
     }
-
     w.Header().Set("Content-Type", "application/json")
     json.NewEncoder(w).Encode(posts)
 }
